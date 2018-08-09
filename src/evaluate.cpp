1da0ba216adcd0537b7c76d6f98c4272c58d133f
Michał Robaczyk <mrobaczyk@man.poznan.pl>
2018-08-09 14:33:06 +0200

    Double rook on file bonus when rook is on the same file as enemy king. Bench: 4800827

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -378,7 +378,7 @@ namespace {
 
             // Bonus for rook on an open or semi-open file
             if (pe->semiopen_file(Us, file_of(s)))
-                score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))];
+                score += RookOnFile[bool(pe->semiopen_file(Them, file_of(s)))] * (1 + (file_of(pos.square<KING>(Them)) == file_of(s)));
 
             // Penalty when trapped by the king, even more if the king cannot castle
             else if (mob <= 3)
