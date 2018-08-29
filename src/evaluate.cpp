6dfeba5f50edf24cef2ed5e2fc5f8e1bad48f6e0
Michał Robaczyk <mrobaczyk@man.poznan.pl>
2018-08-29 08:39:20 +0200

    Tuned values for king squares. Bench: 4707813

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -176,7 +176,7 @@ namespace {
   constexpr Score WeakQueen          = S( 50, 10);
   constexpr Score WeakUnopposedPawn  = S(  5, 29);
 
-  constexpr Score KingNoMobility     = S( 20, 40);
+  constexpr Score KingNoMobility     = S( 22, 41);
 #undef S
 
   // Evaluation class computes and stores attacks tables and other working data
